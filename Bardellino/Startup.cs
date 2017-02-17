bcf163a2c9daf582b84145367e55c3f03089d6d4
cesare <cesare@PC-UFFICIO>
2017-02-17 16:02:24 +0100

    Add project files.

---
+++ Bardellino/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bardellino.Startup))]
+namespace Bardellino
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
